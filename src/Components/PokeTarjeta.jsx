d7317b6094febd3e0eaef6369499357787bbd96e
Nicolas Pergolani <112443088+NicolasPergolani@users.noreply.github.com>
2025-11-12 14:49:39 -0300

    Index y pokCard first commit
    First commit index working with pokeCards on the way 151# pokemons to show

--- src/Components/PokeTarjeta.jsx
+++ src/Components/PokeTarjeta.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Col, Card, CardBody, CardFooter, CardImg, Badge } from 'reactstrap'
 
-const PokeTarjeta = () => {
+const PokeTarjeta = (params) => {
+  const [pokemon, setPokemon] = useState({})
+  const [pokeImg, setPokeImg] = useState('')
+  const [cardClass, setCardClass] = useState('d-none')
+  const [loadClass, setLoadClass] = useState('')
+
+  useEffect(() => {
+    getPokeData();
+  }, []);
+
+  const getPokeData = async () => {
+    try {
+      const liga = params.poke.url
+      const response = await axios.get(liga)
+      const respuesta = response.data
+      setPokemon(respuesta)
+      setPokeImg(respuesta.sprites.front_default)
+      setCardClass('')
+      setLoadClass('d-none')
+    } catch (error) {
+      console.error('Error al obtener datos del pokemon:', error)
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Col sm='4' lg='3' className='mb-3'>
+      <Card>
+        <CardImg src={pokeImg} height='150' className='p-2' />
+        <CardBody className='text-center'>
+          <Badge pill color='danger'>#{pokemon.id}</Badge>
+          <label className='fs-4 text-capitalize'>{pokemon.name}</label>
+        </CardBody>
+      </Card>
+    </Col>
   )
 }
 
--- src/Views/Index.jsx
+++ src/Views/Index.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import fondoImg from "../assets/Fondo.jpg";
+import { Container, InputGroup, InputGroupText, Row, Col, Input } from "reactstrap";
+import PokeTarjeta from "../Components/PokeTarjeta";
 
-const index = () => {
+const Index = () => {
+  console.log('Componente Index montado');
+  const [pokemones, setPokemones] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(151);
+  
+  useEffect(() => {
+    getPokemones(offset);
+  }, []);
+
+  useEffect(() => {
+    console.log('Pokemones actualizados:', pokemones);
+  }, [pokemones]);
+
+  const getPokemones = async (o) => {
+    try {
+      const liga = 'https://pokeapi.co/api/v2/pokemon?limit=' + limit + '&offset=' + o;
+      console.log('Llamando a la API:', liga);
+      const response = await axios.get(liga);
+      const respuesta = response.data;
+      console.log('Respuesta de la API:', respuesta);
+      setPokemones(respuesta.results);
+    } catch (error) {
+      console.error('Error al obtener pokemones:', error);
+    }
+  };
+
   return (
-    <div>
-      Index
-    </div>
-  )
-}
+    <div className="background">
+     <Container className="shadow">  
+       <Row>
+         <Col> 
+            <InputGroup className="mb-3 mt-3 shadow">
+              <InputGroupText>
+                <i className="fa-solid fa-search"></i></InputGroupText>
+              <Input placeholder="Buscar Pokemon" />
+            </InputGroup>        
+          </Col>
+        </Row>
+        <Row>
+          {pokemones.map((pok, i) => (
+            <PokeTarjeta poke={pok} key={i} />
+          ))}    
+        </Row>
+     </Container>
+    </div>     
+  );
+};
 
-export default index
+export default Index;
